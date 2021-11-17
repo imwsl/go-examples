e617de0601ca6e9cfd3d45d03d23e652e24139c7
wesley <wesley.yang789@hotmail.com>
2021-11-17 13:13:54 +0800

    write a http request library with context.Context

--- go.context.test/http-request.go
+++ go.context.test/http-request.go
@@ -2,3 +2,34 @@
 //
 
 package main
+
+import (
+	"fmt"
+	"context"
+	"time"
+)
+
+// 创建一个int的context
+type reqCtx int
+
+func (*reqCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (*reqCtx) Done() <-chan struct{} {
+	return nil
+}
+
+func (*reqCtx) Err() error {
+	return nil
+}
+
+func (*reqCtx) Value(key interface{}) interface{} {
+	return nil
+}
+
+type goReq struct {
+	ctx *reqCtx
+	url
+}
+
